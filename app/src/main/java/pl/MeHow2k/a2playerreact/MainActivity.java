22351119a4e391d69778e1947b42da0a0acd7114
MeHow2k <destroyeverythingpl@gmail.com>
2023-12-20 11:42:16 +0100

    Initial commit

---
+++ app/src/main/java/pl/MeHow2k/a2playerreact/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.MeHow2k.a2playerreact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
